745f46b9eb80bb02fa4db844ae35f3ad14b862e9
rubenbc7 <rubenborbollacastelo@hotmail.com>
2019-08-27 13:37:45 -0700

    piano teclas

--- app.js
+++ app.js
@@ -17,3 +17,13 @@ const playnote =() => {
 buttons.forEach(
     button => 
     button.addEventListener('click', playnote));
+
+     const keyNoteDown = event => {
+         console.log(event);
+         const key = event.key;
+         console.log(key);
+         const button = document.querySelector(`button[data-key="${key}"]`);
+         if(button) button.click();
+     } 
+
+    document.addEventListener('keydown', keyNoteDown);
